4703cbe4bfe70d5ac66d40792047988c77740084
nithishkumarmurugesan2001@gmail.com <nithishkumarmurugesan2001@gmail.com>
2024-08-03 22:07:18 +0530

    Completed Fully

--- DB/dbConnection.js
+++ DB/dbConnection.js
@@ -1,29 +1,29 @@
-import {MongoClient} from "mongodb"
+import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 
 dotenv.config();
 
 //DB setails
-const Db_Name=process.env.DB_NAME;
-const Db_Cluster=process.env.DB_WHOLECLUSTER;
+const Db_Name = process.env.DB_NAME;
+const Db_Cluster = process.env.DB_WHOLECLUSTER;
 
 //DB Cluster
-const url=Db_Cluster;
+const url = Db_Cluster;
 
-const client=new MongoClient(url);
+const client = new MongoClient(url);
 
-const db=client.db(Db_Name);
+const db = client.db(Db_Name);
 
 //DB connection function
-const dbconnection=async()=>{
-    try{
+const dbconnection = async () => {
+    try {
         await client.connect();
         console.log('Connection Successfull');
-    }catch(e){
+    } catch (e) {
         console.log("Connection Failed");
         process.exit(1);
     }
 }
 
-export{db,client};
+export { db, client };
 export default dbconnection;
--- Routing/serviceDetails.js
+++ Routing/serviceDetails.js
@@ -1,18 +1,18 @@
 import Express from "express";
 import { db } from "../DB/dbConnection.js";
 
-const serviceDetails=Express.Router();
+const serviceDetails = Express.Router();
 
 //Types of service db collection
-const serviceDetailsCollection=db.collection(process.env.DB_TYPESOFSERVICES);
+const serviceDetailsCollection = db.collection(process.env.DB_TYPESOFSERVICES);
 
 //API to get the type of service available
-serviceDetails.get("/AllServiceDetails",async(req,res)=>{
-    try{
-        const serviceDetailsData=await serviceDetailsCollection.find({},{projection:{_id:0}}).toArray();
+serviceDetails.get("/AllServiceDetails", async (req, res) => {
+    try {
+        const serviceDetailsData = await serviceDetailsCollection.find({}, { projection: { _id: 0 } }).toArray();
         res.send(serviceDetailsData)
-    }catch(e){
-        res.status(500).send({message:"Internal Server error",e});
+    } catch (e) {
+        res.status(500).send({ message: "Internal Server error", e });
     }
 })
 
